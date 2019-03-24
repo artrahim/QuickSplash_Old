12e12d50b40884f9fccb8d1aba6993690b0a8377
Ahmed <aaa26770123@gmail.com>
2019-03-24 17:16:49 -0600

    Added timer functionality

--- QuestionStage/questions.js
+++ QuestionStage/questions.js
@@ -13,52 +13,6 @@ star.x = 750;
 star.y = 250;
 star.scaleX = 3;
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-let timer = 25;
-
-let blueCircle = new c.Shape()
-    .set({x: 20, y: 20});
-
-blueCircle.graphics.s("blue").ss(1);
-
-let blueStartAngle = 1.5708; // Radian = degrees x pi / 180
-let blueEndAngle = 0;
-
-blueCircle
-    .graphics
-    .setStrokeStyle(10)
-    .arc(0, 0, 45, blueStartAngle, blueEndAngle);
-
-blueCircle.x = 55;
-blueCircle.y = 55;
-
-let redCircle = new c.Shape()
-    .set({x: 20, y: 20});
-
-redCircle.graphics.s("red").ss(1);
-
-redCircle
-    .graphics
-    .setStrokeStyle(10)
-    .arc(0, 0, 45, blueEndAngle, blueStartAngle);
-
-redCircle.x = blueCircle.x;
-redCircle.y = blueCircle.y;
-
-
-let timerText = new c.Text(timer, '50px Arial', "black");
-timerText.x = 27;
-timerText.y = 33;
-
-
-// Group together the counter + timer
-let clock = stage.addChild(new c.Container());
-clock.addChild(blueCircle, redCircle, timerText);
-
-clock.x = 20;
-clock.y = 20;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -88,65 +42,162 @@ item.y = 50;
 
 // SHOULD USE PRELOAD LATER TO LOAD IMAGES...
 
-// Adding different player splashes
+// Adding different smileySplash splashes
 
 red = new Image();
-red.src = "redSplash.png";
+red.src = "images/redSplash.png";
 red.onload = () => {
     // Adding images...
-    let redSplash = new c.Bitmap("redSplash.png");
+    let redSplash = new c.Bitmap(red.src);
     redSplash.x = 30;
     redSplash.y = 600;
-    // redSplash.scale = 0.1
     stage.addChild(redSplash);
     stage.update();
 };
 
 blue = new Image();
-blue.src = "blueSplash.png";
+blue.src = "images/blueSplash.png";
 blue.onload = () => {
-    let blueSplash = new c.Bitmap("blueSplash.png");
+    let blueSplash = new c.Bitmap(blue.src);
     blueSplash.x = 400;
     blueSplash.y = 600;
     stage.addChild(blueSplash);
     stage.update();
 };
+
 smiley = new Image();
-smiley.src = "simley.png";
+smiley.src = "images/simley.png";
 smiley.onload = () => {
-    let smiley = new c.Bitmap("simley.png");
-    smiley.x = 515;
-    smiley.y = 690;
-    stage.addChild(smiley);
+    let smileyFace = new c.Bitmap(smiley.src);
+    smileyFace.x = 515;
+    smileyFace.y = 690;
+    stage.addChild(smileyFace);
     stage.update();
 };
 
 orange = new Image();
-orange.src = "orangeSplash.png";
+orange.src = "images/orangeSplash.png";
 orange.onload = () => {
-    let orangeSplash = new c.Bitmap("orangeSplash.png");
+    let orangeSplash = new c.Bitmap(orange.src);
     orangeSplash.x = 800;
     orangeSplash.y = 600;
     stage.addChild(orangeSplash);
     stage.update();
 };
 
+scared = new Image();
+scared.src = "images/scared.png";
+scared.onload = () => {
+    let scaredFace = new c.Bitmap(scared.src);
+    scaredFace.x = 910;
+    scaredFace.y = 685;
+    stage.addChild(scaredFace);
+    stage.update();
+};
+
 pink = new Image();
-pink.src = "pinkSplash.png";
+pink.src = "images/pinkSplash.png";
 pink.onload = () => {
-    let pinkSplash = new c.Bitmap("pinkSplash.png");
+    let pinkSplash = new c.Bitmap(pink.src);
     pinkSplash.x = 1200;
     pinkSplash.y = 600;
     stage.addChild(pinkSplash);
     stage.update();
 };
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Set timer.
+let time = 20;
+let timeLeftColour = "blue";
+let timeOverColour = "red";
+
+let startAngle = 0.000001 * Math.PI / 180; // Radian = degrees x pi / 180
+let endAngle = 0;
+
+// Arc 1
+let timeLeftCircle = new c.Shape();
+timeLeftCircle.graphics.s(timeLeftColour);
+timeLeftCircle
+    .graphics
+    .setStrokeStyle(10)
+    .arc(0, 0, 45, startAngle, endAngle);
+timeLeftCircle.x = 75; // Position the circle on screen
+timeLeftCircle.y = 75;
+
+// Arc 2
+let timeOverCircle = new c.Shape();
+timeOverCircle.graphics.s(timeOverColour);
+timeOverCircle
+    .graphics
+    .setStrokeStyle(10)
+    .arc(0, 0, 45, endAngle, startAngle);
+timeOverCircle.x = timeLeftCircle.x;
+timeOverCircle.y = timeLeftCircle.y;
+
+let timerText = new c.Text(time, '50px Arial', "black");
+timerText.x = 47;
+timerText.y = 53;
+
+stage.addChild(timeLeftCircle, timeOverCircle, timerText);
+
+stage.update();
+
+decTime = (2 / time) * Math.PI; // How much to decrease each time.
+
+// Timer. Runs every second.
+let timer = setInterval(function () {
+
+    time -= 1; // Decrement time left.
+
+    startAngle += decTime;
+
+    // Remove old circles, and time left
+    stage.removeChild(timeLeftCircle);
+    stage.removeChild(timeOverCircle);
+    stage.removeChild(timerText);
+
+    timerText = new c.Text(time, '50px Arial', "black");
+    if (time < 10) {
+        timerText.x = 62; // Position for single digit num
+    } else
+        timerText.x = 47;
+    timerText.y = 53;
+
+    // Add new ones
+    timeLeftCircle = new c.Shape();
+    timeLeftCircle.graphics.s(timeLeftColour);
+    timeLeftCircle
+        .graphics
+        .setStrokeStyle(10)
+        .arc(75, 75, 45, startAngle, endAngle);
+
+    timeOverCircle = new c.Shape();
+    timeOverCircle.graphics.s(timeOverColour);
+    timeOverCircle
+        .graphics
+        .setStrokeStyle(10)
+        .arc(75, 75, 45, endAngle, startAngle);
+
+    stage.addChild(timeLeftCircle);
+    stage.addChild(timeOverCircle);
+    stage.addChild(timerText);
+
+    stage.update();
+
+    // When timer is complete...
+    if (time === 0)
+        clearInterval(timer);
+
+}, 1000);
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-stage.addChild(clock, star, item);
+stage.addChild(star, item);
 
 stage.update();
 
