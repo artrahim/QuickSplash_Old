413161f39a7b55311559777fc01c6ba90e2b1838
HAMZAHSPC\Hamzah <hamzah.umar@ucalgary.ca>
2019-03-25 14:10:22 -0600

    Update Game.js
    Now displays the question stage of the game.

--- src/Game.js
+++ src/Game.js
@@ -1,151 +1,216 @@
 import React, { Component } from 'react';
-import './Game.css';
 import {socket} from './Main';
-import $ from 'jquery';
 
 class Game extends Component {
 
     componentDidMount(){
-        var user = {
-        	name: "",
-        	nickname: "",
-        	color: ""
-        }
+        let c = window.createjs;
+        var stage = new c.Stage("canvasDisplay");
 
-        $(function () {
+        // Make a star to for teh question and answer.
+        let star = new c.Shape();
+        star.graphics.beginStroke('#000')
+            .setStrokeStyle(0.3)
+            .beginFill("#ccc")
+            .drawPolyStar(0, 0, 240, 10, 0.54);
+        star.x = 750;
+        star.y = 250;
+        star.scaleX = 3;
 
-        	$('form').submit(function(){
-        		socket.emit('chat message', $('#m').val(), user);
-        		$('#m').val('');
-        		return false;
-        	});
 
-        	socket.on('set username', function(name){
-        		//check if cookies exist
-        		//taken from: https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie
-        		if (document.cookie.split(';').filter(function(item){return item.trim().indexOf('name=') == 0}).length) {
-        			//if cookies exist, populate the user variable with the cookies
-        			user.name = parseCookies("name=");
-        			user.nickname = parseCookies("nickname=");
-        			user.color = parseCookies("color=");
-        		}
-        		else {
-        			//if not, set new records
-        			user.name = name;
-        			user.nickname = name;
-        			document.cookie = "name=" + user.name;
-        			document.cookie = "nickname=" + user.nickname;
-        		}
-        		//inform the server that the user joined
-        		socket.emit('join', user);
-        	});
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        	//retrieve and display the chat log
-        	socket.on('retrieve log', function(log){
-        		for (var i=0; i < log.length; i++){
-        			if (log[i].user.name.localeCompare("ERROR") == 0){
-        				continue;
-        			}
-        			appendMessage(log[i]);
-        		}
-        		window.scrollTo(0, document.body.scrollHeight);
-        	});
+        let quest = "Where is the worst place to be naked?";
 
-        	//actions to be taken when a nickname is changed
-        	socket.on('change nickname', function(nickname){
-        		user.nickname = nickname;
-        		document.cookie = "nickname=" + user.nickname;
-        		socket.emit('update user', user);
-        	});
+        let question = new c.Text(quest, "30px Arial");
+        question.x = 340;
+        question.y = 120;
 
-        	//actions to be taken when a color is changed
-        	socket.on('change color', function(color){
-        		user.color = color;
-        		document.cookie = "color=" + user.color;
-        		socket.emit('update user', user);
-        	});
+        let ans = "_________________________________";
 
-        	//action to be taken when the list of online users needs to be updated
-        	socket.on('list users', function(users){
-        		$('#users').empty();
-        		for (var i=0; i < users.length; i++){
-        			//add "(YOU)" is the user being listed is the current user
-        			if (users[i].name.localeCompare(user.name) == 0){
-        				$('#users').append($('<li>' + users[i].nickname + ' (YOU)</li>'));
-        			}
-        			else{
-        				$('#users').append($('<li>' + users[i].nickname + '</li>'));
-        			}
-        		}
-        	});
+        let answer = new c.Text(ans, "30px Arial");
+        answer.x = 330;
+        answer.y = 230;
 
-        	//action to be taken when a message has been sent
-        	socket.on('chat message', function(message){
-        		appendMessage(message);
-        		window.scrollTo(0, document.body.scrollHeight);
-        	});
 
-        });
+        // Group together the question and answer.
+        let item = stage.addChild(new c.Container());
+        item.addChild(question, answer);
 
-        //fucntion to append a message to the message display
-        function appendMessage(message){
-        	//retrieve the message information
-        	var messageText = message.text;
-        	var hour = message.hour;
-        	var minute = message.minute;
-        	var second = message.second;
-        	var name = message.user.name;
-        	var color = message.user.color;
-        	var nickname = message.user.nickname;
-        	//format the time
-        	if (hour < 10){
-        		hour = '0' + hour;
-        	}
-        	if (minute < 10){
-        		minute = '0' + minute;
-        	}
-        	if (second < 10){
-        		second = '0' + second;
-        	}
-        	//print all messages that are not error messages
-        	if (name.localeCompare(user.name) == 0){
-        		//bold messages that are sent by the current user
-        		$('#messages').append($('<li>' + hour + ':' + minute + ':' + second +
-        		'  ' + '<span style="color:' + color + '">' + nickname + '</span>' + ':  <b>' + messageText + '</b></li>'));
-        	}
-        	else{
-        		$('#messages').append($('<li>' + hour + ':' + minute + ':' + second +
-        		'  ' + '<span style="color:' + color + '">' + nickname + '</span>' + ':  ' + messageText + '</li>'));
-        	}
-        }
+        item.x = 140;
+        item.y = 50;
 
-        //function to retrieve cookies
-        function parseCookies(cookie){
-        	var cookies = document.cookie.split('; ');
-        	var value = "";
-        	for (var i=0; i < cookies.length; i++){
-        		if (cookies[i].startsWith(cookie)){
-        			var temp = cookies[i].split('=');
-        			value = temp[1];
-        		}
-        	}
-        	return value;
-        }
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // SHOULD USE window.window.preload LATER TO LOAD IMAGES...
+
+        // Adding different smileySplash splashes
+
+        var red = new Image();
+        red.src = "./images/redSplash.png";
+        red.onload = () => {
+            // Adding images...
+            let redSplash = new c.Bitmap(red.src);
+            redSplash.x = 30;
+            redSplash.y = 600;
+            stage.addChild(redSplash);
+            stage.update();
+        };
+
+        var blue = new Image();
+        blue.src = "./images/blueSplash.png";
+        blue.onload = () => {
+            let blueSplash = new c.Bitmap(blue.src);
+            blueSplash.x = 400;
+            blueSplash.y = 600;
+            stage.addChild(blueSplash);
+            stage.update();
+        };
+
+        var smiley = new Image();
+        smiley.src = "./images/simley.png";
+        smiley.onload = () => {
+            let smileyFace = new c.Bitmap(smiley.src);
+            smileyFace.x = 515;
+            smileyFace.y = 690;
+            stage.addChild(smileyFace);
+            stage.update();
+        };
+
+        var orange = new Image();
+        orange.src = "./images/orangeSplash.png";
+        orange.onload = () => {
+            let orangeSplash = new c.Bitmap(orange.src);
+            orangeSplash.x = 800;
+            orangeSplash.y = 600;
+            stage.addChild(orangeSplash);
+            stage.update();
+        };
+
+        var scared = new Image();
+        scared.src = "./images/scared.png";
+        scared.onload = () => {
+            let scaredFace = new c.Bitmap(scared.src);
+            scaredFace.x = 910;
+            scaredFace.y = 685;
+            stage.addChild(scaredFace);
+            stage.update();
+        };
+
+        var pink = new Image();
+        pink.src = "./images/pinkSplash.png";
+        pink.onload = () => {
+            let pinkSplash = new c.Bitmap(pink.src);
+            pinkSplash.x = 1200;
+            pinkSplash.y = 600;
+            stage.addChild(pinkSplash);
+            stage.update();
+        };
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // Set timer.
+        let time = 20;
+        let timeLeftColour = "blue";
+        let timeOverColour = "red";
+
+        let startAngle = 0.000001 * Math.PI / 180; // Radian = degrees x pi / 180
+        let endAngle = 0;
+
+        // Arc 1
+        let timeLeftCircle = new c.Shape();
+        timeLeftCircle.graphics.s(timeLeftColour);
+        timeLeftCircle
+            .graphics
+            .setStrokeStyle(10)
+            .arc(0, 0, 45, startAngle, endAngle);
+        timeLeftCircle.x = 75; // Position the circle on screen
+        timeLeftCircle.y = 75;
+
+        // Arc 2
+        let timeOverCircle = new c.Shape();
+        timeOverCircle.graphics.s(timeOverColour);
+        timeOverCircle
+            .graphics
+            .setStrokeStyle(10)
+            .arc(0, 0, 45, endAngle, startAngle);
+        timeOverCircle.x = timeLeftCircle.x;
+        timeOverCircle.y = timeLeftCircle.y;
+
+        let timerText = new c.Text(time, '50px Arial', "black");
+        timerText.x = 47;
+        timerText.y = 53;
+
+        stage.addChild(timeLeftCircle, timeOverCircle, timerText);
+
+        stage.update();
+
+        var decTime = (2 / time) * Math.PI; // How much to decrease each time.
+
+        // Timer. Runs every second.
+        let timer = setInterval(function () {
+
+            time -= 1; // Decrement time left.
+
+            startAngle += decTime;
+
+            // Remove old circles, and time left
+            stage.removeChild(timeLeftCircle);
+            stage.removeChild(timeOverCircle);
+            stage.removeChild(timerText);
+
+            timerText = new c.Text(time, '50px Arial', "black");
+            if (time < 10) {
+                timerText.x = 62; // Position for single digit num
+            } else
+                timerText.x = 47;
+            timerText.y = 53;
+
+            // Add new ones
+            timeLeftCircle = new c.Shape();
+            timeLeftCircle.graphics.s(timeLeftColour);
+            timeLeftCircle
+                .graphics
+                .setStrokeStyle(10)
+                .arc(75, 75, 45, startAngle, endAngle);
+
+            timeOverCircle = new c.Shape();
+            timeOverCircle.graphics.s(timeOverColour);
+            timeOverCircle
+                .graphics
+                .setStrokeStyle(10)
+                .arc(75, 75, 45, endAngle, startAngle);
+
+            stage.addChild(timeLeftCircle);
+            stage.addChild(timeOverCircle);
+            stage.addChild(timerText);
+
+            stage.update();
+
+            // When timer is complete...
+            if (time === 0)
+                clearInterval(timer);
+
+        }, 1000);
+
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+        stage.addChild(star, item);
+
+        stage.update();
 
     }
 
     render() {
         return (
-            <div id="main">
-                <div id="messageDisplay">
-                    <ul id="messages" />
-                </div>
-                <div id="usersDisplay">
-                    <ul id="users" />
-                </div>
-                <form action>
-                    <input id="m" autoComplete="off" />
-                </form>
+            <div className="display">
+                <canvas id="canvasDisplay" width={1500} height={1000} />
             </div>
         );
     }
